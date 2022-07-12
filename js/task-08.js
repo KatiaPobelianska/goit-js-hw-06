8da2ab1330ea66425b5d928729309401f1eda7bc
KatiaPobelianska <alieva957@gmail.com>
2022-07-12 18:20:34 +0300

    resolve 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,3 +1,18 @@
 "use strict";
 
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Заполни форму!");
+    return;
+  }
+  console.log({
+    email: `${email.value}`,
+    password: `${password.value}`,
+  });
+  form.reset();
+});
